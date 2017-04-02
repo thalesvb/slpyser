821b8b728f7d74ef8ade52c294a8077f9401ee6a
thalesvb <thalesvb@live.com>
2017-04-02 08:29:38 -0300

    Validate some facts about programs.

--- tests/program_test.py
+++ tests/program_test.py
@@ -6,10 +6,19 @@ from context import slpyser, Util
 class TestProgram(unittest.TestCase):
 
 
-    def test_program(self):
+    def test_simple_program(self):
+        SIMPLE_PROGRAM_NAME = 'ZSLPUT_PROGRAM_SIMPLE'
         parsed_data = slpyser.parse(Util.file_path("NUGG_SLPUT_PROGRAMS.nugg"))
-        programs_names = parsed_data.programs.keys()
-        self.assertIn('ZSLPUT_PROGRAM_SIMPLE', programs_names)
+        programs = parsed_data.programs
+        programs_names = programs.keys()
+        # The program should exists in the parsed list.
+        self.assertIn(SIMPLE_PROGRAM_NAME, programs_names)
+        
+        simple_program = programs.get(SIMPLE_PROGRAM_NAME)
+        # Some source code should exists in the program.
+        self.assertGreater(len(simple_program.source_code.source_code), 0)
+        # At least one text pool language should exists for a program.
+        self.assertGreater(len(simple_program.text_pool.language_mapping), 0)
 
 
 if __name__ == '__main__':
